7b07e9535b6c82f01593c4c8096c15d20ab6871e
Jamie Fletcher <jamie@unified-streaming.com>
2025-09-18 19:41:01 +0200

    backup repo

---
+++ src/build_simple/CMakeFiles/simple_server.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/jamie/repos/github-personal/Adafruit_Blinka_Raspberry_Pi5_Piomatter/src/piolib/pio_rp1.c" "CMakeFiles/simple_server.dir/Users/jamie/repos/github-personal/Adafruit_Blinka_Raspberry_Pi5_Piomatter/src/piolib/pio_rp1.c.o" "gcc" "CMakeFiles/simple_server.dir/Users/jamie/repos/github-personal/Adafruit_Blinka_Raspberry_Pi5_Piomatter/src/piolib/pio_rp1.c.o.d"
+  "/Users/jamie/repos/github-personal/Adafruit_Blinka_Raspberry_Pi5_Piomatter/src/piolib/piolib.c" "CMakeFiles/simple_server.dir/Users/jamie/repos/github-personal/Adafruit_Blinka_Raspberry_Pi5_Piomatter/src/piolib/piolib.c.o" "gcc" "CMakeFiles/simple_server.dir/Users/jamie/repos/github-personal/Adafruit_Blinka_Raspberry_Pi5_Piomatter/src/piolib/piolib.c.o.d"
+  "/Users/jamie/repos/github-personal/Adafruit_Blinka_Raspberry_Pi5_Piomatter/src/simple_websocket_server.cpp" "CMakeFiles/simple_server.dir/Users/jamie/repos/github-personal/Adafruit_Blinka_Raspberry_Pi5_Piomatter/src/simple_websocket_server.cpp.o" "gcc" "CMakeFiles/simple_server.dir/Users/jamie/repos/github-personal/Adafruit_Blinka_Raspberry_Pi5_Piomatter/src/simple_websocket_server.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/build_simple/CMakeFiles/simple_server.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simple_server.dir/Users/jamie/repos/github-personal/Adafruit_Blinka_Raspberry_Pi5_Piomatter/src/piolib/pio_rp1.c.o"
+  "CMakeFiles/simple_server.dir/Users/jamie/repos/github-personal/Adafruit_Blinka_Raspberry_Pi5_Piomatter/src/piolib/pio_rp1.c.o.d"
+  "CMakeFiles/simple_server.dir/Users/jamie/repos/github-personal/Adafruit_Blinka_Raspberry_Pi5_Piomatter/src/piolib/piolib.c.o"
+  "CMakeFiles/simple_server.dir/Users/jamie/repos/github-personal/Adafruit_Blinka_Raspberry_Pi5_Piomatter/src/piolib/piolib.c.o.d"
+  "CMakeFiles/simple_server.dir/Users/jamie/repos/github-personal/Adafruit_Blinka_Raspberry_Pi5_Piomatter/src/simple_websocket_server.cpp.o"
+  "CMakeFiles/simple_server.dir/Users/jamie/repos/github-personal/Adafruit_Blinka_Raspberry_Pi5_Piomatter/src/simple_websocket_server.cpp.o.d"
+  "protomatter.pio.h"
+  "simple_server"
+  "simple_server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/simple_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
